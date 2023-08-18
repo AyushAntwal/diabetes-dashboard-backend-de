0cf08311005fa500e7cfdf91e8a788c9e6175e9d
Ayush Antiwal <91528325+AyushAntwal@users.noreply.github.com>
2023-08-18 17:55:14 +0530

    Update server

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+  next();
+});
 
 
 app.use("/backend", mainRoute);
