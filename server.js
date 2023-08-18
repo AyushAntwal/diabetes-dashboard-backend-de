c0212773e2ff1551cb061d4c0a10aff975cffacc
Ayush Antiwal <91528325+AyushAntwal@users.noreply.github.com>
2023-08-18 14:29:40 +0530

    Hosting To new server test 3

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan")
 app.use(cors());
+app.use(morgan('common'));
 require("./MongoDB/db");
-
 var bodyParser = require("body-parser");
 const mainRoute = require("./Routes/main");
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
 
-app.use("/", mainRoute);
+
+
+
+
+app.use("/backend", mainRoute);
 
 app.listen(2100, () => {
   console.log("Server Is running on ", 2100);
